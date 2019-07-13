8d196a50f4104cfc9e9090fe8eed8c77d5b82000
Vicente Chavez <vchavez92780@gmail.com>
2019-07-12 23:50:03 -0400

    Added function to process async array

--- utils.js
+++ utils.js
@@ -12,6 +12,12 @@ exports.logError = (msg) => {
     return clc.red(msg)
 }
 
-exports.logAlert= (msg) => {
+exports.logAlert = (msg) => {
     return clc.red(msg)
+}
+
+exports.asyncForEach = async(array, callback) => {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array)
+    }
 }
