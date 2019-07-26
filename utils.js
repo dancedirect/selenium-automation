3b0a15f843b31af529931e92773265f41190d7fb
Vicente Chavez <vchavez92780@gmail.com>
2019-07-25 20:19:58 -0400

    Added functionality to exist async loop

--- utils.js
+++ utils.js
@@ -20,7 +20,10 @@ exports.logAlert = (msg) => {
 
 exports.asyncForEach = async (array, callback) => {
     for (let index = 0; index < array.length; index++) {
-        await callback(array[index], index, array)
+        const ret = await callback(array[index], index, array)
+        if (ret === false) {
+            break
+        }
     }
 }
 
